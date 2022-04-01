65970826250a2b76de2225f191cd3cce5244cb21
RafaelBalbino <rafaelballabi@hotmail.com>
2022-04-01 15:49:37 -0300

    Atualizado em 01.04

--- exercicios1a4/Exercicio2.java
+++ exercicios1a4/Exercicio2.java
@@ -4,25 +4,50 @@ package exercicios1a4;
 import java.util.Scanner;
 
 public class Exercicio2 {
-	public static void main(String[] args) {
-			int n1, n2, n3;
+
+	public static void main(String[] args) 
+	{
+        Scanner num = new Scanner(System.in);
+        
+		int n1,n2,n3;
+				
+		System.out.println("Entre com o primeiro número: ");
+		n1=num.nextInt();
 		
-			Scanner leia = new Scanner(System.in);
-			
-			System.out.println("Digite o primeiro número: ");
-			n1 = leia.nextInt();
-			
-			System.out.println("Digite o segundo número: ");
-			n2 = leia.nextInt();
-			
-			System.out.println("Digite o terceiro número: ");
-			n3 = leia.nextInt();
-			
-			
-			
-			System.out.println("FIM DO PROGRAMA!");
-	}
+		System.out.println("Entre com o segundo número: ");
+		n2=num.nextInt();
 		
-		
-		
-}
+		System.out.println("Entre com o terceiro número: ");
+		n3=num.nextInt();
+
+		if(n1<n2)
+		{
+			if(n2<n3)
+			{
+				System.out.println("A ordem crescente correta é: " + (n1) + " " + (n2) + " " + (n3));
+			}
+			else if(n1<n3) 
+			{
+				System.out.println("A ordem crescente correta é: "+(n1) + (n3) + (n2));
+			} 
+			else 
+			{
+				System.out.println("A ordem crescente correta é: "+(n3) + (n1) + (n2));
+			}
+		}
+			else if(n2<n3) 
+			{
+				if(n1<n3) 
+				{
+					System.out.println("A ordem crescente correta é: "+(n2) + (n1) + (n3));
+				}else 
+				{
+					System.out.println("A ordem crescente correta é: "+(n2) + (n3) + (n1));
+				}
+			} 
+			else 
+			{
+				System.out.println("A ordem crescente correta é: "+(n3) + (n2) + (n1));
+			}
+	     }
+     }
